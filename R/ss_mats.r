c5da390ef4a6429daddac12994851a422f60d99f
Damian Pavlyshyn <damian.pavlyshyn@gmail.com>
2023-02-02 17:12:29 +1100

    Substantially speed up computation of sum-of-squares matrices
    The calculations have been vectorised and no longer use tidyverse functions

--- R/ss_mats.r
+++ R/ss_mats.r
@@ -54,24 +54,38 @@ ss_mats <- function(df) {
   J <- (df$dam %>% unique %>% length) / I
   K <- (df$individual %>% unique %>% length) / (I*J)
 
-  df_means <- df %>%
-    dplyr::group_by(.data$trait) %>%
-    dplyr::mutate(grand = mean(.data$value)) %>%
-    dplyr::group_by(.data$trait, .data$sire) %>%
-    dplyr::mutate(sire = mean(.data$value)) %>%
-    dplyr::group_by(.data$trait, .data$sire, .data$dam) %>%
-    dplyr::mutate(dam = mean(.data$value)) %>%
-    dplyr::ungroup()
+  ## df_means <- df %>%
+  ##   dplyr::group_by(.data$trait) %>%
+  ##   dplyr::mutate(grand = mean(.data$value)) %>%
+  ##   dplyr::group_by(.data$trait, .data$sire) %>%
+  ##   dplyr::mutate(sire = mean(.data$value)) %>%
+  ##   dplyr::group_by(.data$trait, .data$sire, .data$dam) %>%
+  ##   dplyr::mutate(dam = mean(.data$value)) %>%
+  ##   dplyr::ungroup()
 
-  vals <- df_means$value %>% matrix(nrow = q)
-  grand_mean <- df_means$grand %>% matrix(nrow = q)
-  sire_mean <- df_means$sire %>% matrix(nrow = q)
-  dam_mean <- df_means$dam %>% matrix(nrow = q)
+  ## vals <- df_means$value %>% matrix(nrow = q)
+  ## grand_mean <- df_means$grand %>% matrix(nrow = q)
+  ## sire_mean <- df_means$sire %>% matrix(nrow = q)
+  ## dam_mean <- df_means$dam %>% matrix(nrow = q)
+
+  X <- t(matrix(df$value, nrow = q))
+
+  dam_means  <- rowsum(X, group = rep(1:(I * J), each = K)) / K
+  sire_means <- rowsum(dam_means, group = rep(1:I, each = J)) / J
+  grand_mean <- colMeans(sire_means)
+
+  ind_mom <- t(X) %*% X
+  dam_mom <- t(dam_means) %*% dam_means
+  sire_mom <- t(sire_means) %*% sire_means
+  grand_mom <- grand_mean %o% grand_mean
 
   list(
-    m_ind = (vals - dam_mean) %*% t(vals - dam_mean) / (I*J*(K-1)),
-    m_dam = (dam_mean - sire_mean) %*% t(dam_mean - sire_mean) / (I*(J-1)),
-    m_sire = (sire_mean - grand_mean) %*% t(sire_mean - grand_mean) / (I-1),
+    ## m_ind = (vals - dam_mean) %*% t(vals - dam_mean) / (I*J*(K-1)),
+    ## m_dam = (dam_mean - sire_mean) %*% t(dam_mean - sire_mean) / (I*(J-1)),
+    ## m_sire = (sire_mean - grand_mean) %*% t(sire_mean - grand_mean) / (I-1),
+    m_ind = 1 / (I * J * (K-1)) * (ind_mom - K * dam_mom),
+    m_dam = K / (I * (J - 1)) * (dam_mom - J * sire_mom),
+    m_sire = (J * K) / (I - 1) * (sire_mom - I * grand_mom),
     I = I,
     J = J,
     K = K,
